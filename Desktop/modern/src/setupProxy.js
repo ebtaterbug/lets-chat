a7aed722768647518fc49c50e66c47491ea7aee1
Ian Joseph <89359346+ebtaterbug@users.noreply.github.com>
2022-07-05 17:02:51 -0700

    Delete Desktop directory

--- Desktop/modern/src/setupProxy.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable import/no-extraneous-dependencies */
-
-const proxy = require('http-proxy-middleware');
-
-module.exports = (app) => {
-  app.use(proxy('/api/socket', { target: `ws://${process.env.REACT_APP_URL_NAME}`, ws: true }));
-  app.use(proxy('/api', { target: `http://${process.env.REACT_APP_URL_NAME}` }));
-};
